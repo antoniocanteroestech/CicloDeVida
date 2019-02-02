7d3e216c09f2e8cc81be28873169f70223ac0b24
Antonio Cantero Ruiz <acanteroruiz.dev@gmail.com>
2019-02-02 09:08:01 +0100

    changed Toast by Log

--- app/src/main/java/es/estech/ciclodevida/MainActivity.java
+++ app/src/main/java/es/estech/ciclodevida/MainActivity.java
@@ -2,45 +2,47 @@ package es.estech.ciclodevida;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final String TAG = "CICLO DE VIDA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onCreate");
     }
 
     @Override protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStart");
     }
 
     @Override protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onResume");
     }
 
     @Override protected void onPause() {
-        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
         super.onPause();
     }
 
     @Override protected void onStop() {
-        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onStop");
         super.onStop();
     }
 
     @Override protected void onRestart() {
         super.onRestart();
-        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onRestart");
     }
 
     @Override protected void onDestroy() {
-        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onDestroy");
         super.onDestroy();
     }
 }
